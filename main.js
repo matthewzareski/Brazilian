709806a4681e82aaaeb82c74aa869d509210b552
matthewzareski <78160277+matthewzareski@users.noreply.github.com>
2021-10-02 12:11:33 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+nosex = 0;
+nosey = 0;
+
+function preload() {
+    nose = loadImage('https://i.postimg.cc/LXf9w70f/musdashe.png')
+}
+
+function setup() {
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    posenet=ml5.poseNet(video, modelLoaded);
+    posenet.on('pose', gotposes);
+}
+
+function draw(){
+image(video, 0,0,300,300);
+image(nose,nosex-5,nosey+15,40,30);
+//filter(INVERT); will invert the image colors/colours
+}
+
+function take_snapshot(){
+    save('UwU.png')
+}
+
+function modelLoaded(){
+    console.log("model was initialised lol");
+}
+
+function gotposes(result){
+if(result.length > 0){
+console.log(result);
+console.log('nose x='+result[0].pose.nose.x);
+console.log('nose y='+result[0].pose.nose.y);
+nosex = result[0].pose.nose.x -15;
+nosey = result[0].pose.nose.y -15;
+}
+}
